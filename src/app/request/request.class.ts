d04134c769d00f2b06958091c74a0b7487034c32
Megan Moore <meganmoore.dev@gmail.com>
2023-10-19 16:06:29 -0400

    request update

--- src/app/request/request.class.ts
+++ src/app/request/request.class.ts
@@ -6,7 +6,7 @@ export class Request {
     justification: string = "";
     rejectionReason: string = "";
     deliveryMode: string = "Pickup";
-    status: string = "";
+    status: string = "NEW";
     total: number = 0;
 
     userId: number = 0;
